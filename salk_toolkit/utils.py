Fix a bug with replace meta, handle json error in explorer and tweak map custom color gradients

salk_toolkit/utils.py
@@ -6,10 +6,10 @@
 __all__ = ['warn', 'default_color', 'default_bidirectional_gradient', 'redblue_gradient', 'factorize_w_codes', 'batch',
            'loc2iloc', 'match_sum_round', 'min_diff', 'continify', 'replace_cat_with_dummies', 'match_data',
            'replace_constants', 'approx_str_match', 'index_encoder', 'to_alt_scale', 'multicol_to_vals_cats',
-           'gradient_to_discrete_color_scale', 'gradient_subrange', 'gradient_from_color', 'bidir_gradient_from_color',
-           'is_datetime', 'rel_wave_times', 'stable_draws', 'deterministic_draws', 'clean_kwargs', 'censor_dict',
-           'cut_nice_labels', 'cut_nice', 'rename_cats', 'str_replace', 'merge_series', 'aggregate_multiselect',
-           'deaggregate_multiselect', 'gb_in', 'gb_in_apply', 'stk_defaultdict', 'cached_fn']
+           'gradient_to_discrete_color_scale', 'gradient_subrange', 'gradient_from_color', 'is_datetime',
+           'rel_wave_times', 'stable_draws', 'deterministic_draws', 'clean_kwargs', 'censor_dict', 'cut_nice_labels',
+           'cut_nice', 'rename_cats', 'str_replace', 'merge_series', 'aggregate_multiselect', 'deaggregate_multiselect',
+           'gb_in', 'gb_in_apply', 'stk_defaultdict', 'cached_fn']
 
 # %% ../nbs/10_utils.ipynb 3
 import json, os, warnings, math, inspect
@@ -224,25 +224,18 @@ def gradient_subrange(grad, num_colors, range=[-1,1], bidirectional=True):
 def gradient_from_color(color, l_value=0.3, n_points=7, range=[0,1]):
     if n_points==0: return [] # Special case useful for bidir below
 
-    # Set max luminosity to be slightly below pure white
-    max_l = 95
+    # Get hue and saturation for color (ignoring luminosity to make scales uniform on that)    
+    ch,cs,_ = hsluv.hex_to_hsluv(mpc.to_hex(color))
 
-    h,s,l = hsluv.hex_to_hsluv(mpc.to_hex(color))
-    s = min(s,70) # Desaturate the color as it can be very agressive on high L values otherwise
-    maxv = max_l-min(l,100*l_value)
-    mi, ma = range[0]*maxv, range[1]*maxv
-    ls = np.linspace(max_l-mi,max_l-ma,n_points) # Create n_points steps in hsluv space
-    return [ hsluv.hsluv_to_hex((h,s,nl)) for nl in ls ]
+    max_l = 94 # Set max luminosity to be slightly below pure white
+    l_diff = max_l - 100*l_value # Difference between max and min luminosity
 
-def bidir_gradient_from_color(ncol, pcol, l_value=0.3, n_points=11, range=[-1,1]):
-    ami, ama = max(0,-range[0]), max(0,range[1])
-    wneg, wpos = ami/(ami+ama), ama/(ami+ama)
-    n_neg, n_pos = round((n_points)*wneg), round((n_points)*wpos)
+    beg_s, end_s = 3*cs*range[0], 3*cs*range[1] # As we use min(cs, s), this just desaturates on first 1/3 of the range
+    beg_l, end_l = max_l-l_diff*range[0], max_l-l_diff*range[1]
 
-    ng = gradient_from_color(ncol, l_value, n_neg, [max(0,-range[1]),ami])
-    pg = gradient_from_color(pcol, l_value, n_pos, [max(0,range[0]),ama])
-    if n_neg and n_pos: return ng[::-1] + pg[1:]
-    else: return ng[::-1] + pg
+    ls = np.linspace(0,1,n_points) # Create n_points steps in hsluv space
+    return [ hsluv.hsluv_to_hex((ch,min(cs,w*end_s+(1-w)*beg_s),(w*end_l+(1-w)*beg_l))) for w in ls ]
+
 
 # %% ../nbs/10_utils.ipynb 27
 def is_datetime(col):